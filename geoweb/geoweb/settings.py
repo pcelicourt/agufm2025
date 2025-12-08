7dae016bdad3029973b54d247e29e07c74189520
Paul Celicourt <celicourtpaul@gmail.com>
2025-12-08 22:41:23 +0000

    Add GDAL and GEOS library path configurations to settings.py

--- geoweb/geoweb/settings.py
+++ geoweb/geoweb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from ctypes.util import find_library
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +123,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+GDAL_LIBRARY_PATH = find_library("gdal")
+GEOS_LIBRARY_PATH = find_library("geos_c")
+SPATIALITE_LIBRARY_PATH = "mod_spatialite"
