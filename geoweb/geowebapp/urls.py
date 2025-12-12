c53a07906af462c0ee5489d2034fd72cecfcf2fe
Paul Celicourt <celicourtpaul@gmail.com>
2025-12-12 02:28:01 +0000

    Refactor geolocation functionality: remove unused methods, update URL patterns, and implement user location storage

--- geoweb/geowebapp/urls.py
+++ geoweb/geowebapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('staticmap/', views.map_view, name='map_view'),
     path('farm/', views.MainView.as_view(), name='farm'),
     path('farm/sensors/sensor', views.map_view, name='sensor'),
+    path('user-location/', views.store_user_location, name='user_location'),
+    path('location-from-ip/', views.location_from_ip, name='location_from_ip'),
 ]
--- geoweb/geowebapp/views.py
+++ geoweb/geowebapp/views.py
@@ -5,20 +5,26 @@ from django.utils.safestring import SafeString
 from django.utils.safestring import mark_safe
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
-from pyproj import Transformer
-from shapely.wkt import loads as load_wkt
-from shapely.ops import transform
-
-from django.views.generic import TemplateView
 from django.http import JsonResponse
+from django.views.generic import TemplateView
+import uuid
+
+from pyproj import CRS, Transformer
+from shapely.wkt import loads as load_wkt
+#from shapely.ops import transform
+from shapely.geometry import Point
+
+import requests
 import folium
 from folium.map import Marker, Template
 
 import json
 
-from .models import SamplingFeatures
+from .models import SamplingFeatures, TimeSeriesResults, \
+    TimeSeriesResultValues, Results, CV_SamplingFeatureGeoType, CV_SamplingFeatureType
 from .serializers import SamplingFeaturesSerializers
 
+from .utils import get_client_ip
 
 def map_view(request):
     geos = []
@@ -223,3 +229,80 @@ class MainView(TemplateView):
         sensors_json = JsonResponse(
             SamplingFeaturesSerializers(sensors, many=True).data).content
         return {"map": figure._repr_html_(), 'title': 'Cook Agronomy Farm', 'sensors': sensors_json}
+    
+
+userlocationfeaturegeotypecv = CV_SamplingFeatureGeoType.objects.filter(
+    term='point').first()
+
+sampling_feature_type_cv = CV_SamplingFeatureType.objects.filter(
+    term="site").first()
+
+
+@ensure_csrf_cookie
+def store_user_location(request):
+    if request.method == 'POST':
+        latitude = request.POST.get('latitude')
+        longitude = request.POST.get('longitude')
+        timestamp = request.POST.get('timestamp')
+        print(latitude, longitude, timestamp)
+        crs_transformer = Transformer.from_crs(4326, 3857, always_xy=True)
+        longitude, latitude = crs_transformer.transform(
+            float(longitude), float(latitude))
+        point = Point(float(longitude), float(latitude))
+        wkt = point.wkt
+        print(wkt)
+        # Save into the DB
+        user_location = SamplingFeatures(
+            samplingfeatureuuid=str(uuid.uuid4()),
+            samplingfeaturename=f'User Location {timestamp}',
+            samplingfeaturetypecv=sampling_feature_type_cv,
+            samplingfeaturecode=f'User_{timestamp}',
+            samplingfeaturedescription='User Location from Browser',
+            featuregeometry=wkt,
+            featuregeometrywkt=wkt,
+            samplingfeaturegeotypecv=userlocationfeaturegeotypecv,
+            elevation_m=-9999,
+            # elevationdatumcv=elevation_datum_cv,
+        )
+        user_location.save()
+        all_features = SamplingFeatures.objects.filter(
+            samplingfeaturedescription='User Location from Browser')  # QuerySet
+        last_feature = all_features[::-1][0]
+        print("Last feature saved:", last_feature.featuregeometry)
+        return JsonResponse({'status': 'success', 'latitude': latitude, 'longitude': longitude})
+    elif request.method == 'GET':
+        # Temporarily handle GET for debugging
+        return JsonResponse({
+            'status': 'error',
+            'message': 'POST request expected but received GET',
+            'method': request.method
+        })
+
+    return JsonResponse({'status': 'invalid request', 'method': request.method}, status=400)
+
+
+def get_geolocation_data(ip_address):
+    api_key = 'a61126b08a4c45528b8f60aa276a2671'
+    url = f'https://ipgeolocation.abstractapi.com/v1/?api_key={api_key}&ip_address={ip_address}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            'city': data.get('city'),
+            'region': data.get('region'),
+            'country': data.get('country'),
+            'latitude': data.get('latitude'),
+            'longitude': data.get('longitude'),
+        }
+
+    return None
+
+
+def location_from_ip(request):
+    user_ip = get_client_ip(request)
+    location_data = get_geolocation_data(user_ip)
+    if location_data:
+        # Store or personalize based on location_data
+        return JsonResponse({'location': location_data})
+    return JsonResponse({'error': 'Location not found'}, status=404)
+
