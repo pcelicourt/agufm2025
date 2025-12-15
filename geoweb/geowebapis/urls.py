3fe4da4e567de036e2b8a03957903d31c601ecf7
Paul Celicourt <celicourtpaul@gmail.com>
2025-12-15 21:42:04 +0000

    Uncomment URL patterns in geowebapp and geowebapis to enable routing for user location and sensor data features

--- geoweb/geowebapis/urls.py
+++ geoweb/geowebapis/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-#from .views import sensor_data, sampling_features
+from .views import sensor_data, sampling_features
 
 
 urlpatterns = [
-    #path('sensor/', sensor_data, name='sensor'),
-    #path('samplingfeatures/', sampling_features, name='samplingfeatures'),    
+    path('sensor/', sensor_data, name='sensor'),
+    path('samplingfeatures/', sampling_features, name='samplingfeatures'),    
 ] 
