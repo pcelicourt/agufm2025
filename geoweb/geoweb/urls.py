966a062a525146546a320f17db5fb6bbdb447fde
Paul Celicourt <celicourtpaul@gmail.com>
2026-03-20 04:20:51 +0000

    add support for a simple web service demonstration

--- geoweb/geoweb/urls.py
+++ geoweb/geoweb/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path('', include('geowebapp.urls')),
+    path('api/', include('geowebapis.urls')),
+    path('admin/', admin.site.urls),
 ]
--- geoweb/geowebapis/serializer.py
+++ geoweb/geowebapis/serializer.py
@@ -1,19 +1,31 @@
 from rest_framework_gis import serializers
+
 from geowebapis.models import NonSpatialCities, Countries, Cities
-from django.http import JsonResponse
+from geowebapp.models import SamplingFeatures
 
 
 class NonSpatialCitiesModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = NonSpatialCities
-        fields = ['city_id', 'city_name', 'country', 'population', 'geolocation', 'province']
+        fields = ['city_id', 'city_name', 'country',
+                  'population', 'geolocation', 'province']
 
 
 class CitiesSerializers(serializers.GeoFeatureModelSerializer):
     """ A class to serialize locations as GeoJSON compatible data """
 
     class Meta:
-        app_label  = 'geowebapis'
+        app_label = 'geowebapis'
         model = Cities
         geo_field = "location"
         fields = ('city_id', 'city_name', 'country_id_id')
+
+
+class SamplingFeatureSerializers(serializers.GeoFeatureModelSerializer):
+    """ A class to serialize locations as GeoJSON compatible data """
+
+    class Meta:
+        app_label = 'geowebapis'
+        model = SamplingFeatures
+        geo_field = "featuregeometry"
+        fields = '__all__'
